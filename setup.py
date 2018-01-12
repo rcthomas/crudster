a1b3389672d7f1c561dd0197c489e3eb421df9de
Rollin Thomas <rcthomas@lbl.gov>
2018-01-12 08:13:41 -0800

    Build script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name="crudster",
+        version="0.0.1",
+        description="Simple Motor-based document-store REST API",
+        author="R. C. Thomas",
+        author_email="rcthomas@lbl.gov",
+        url="https://github.com/rcthomas/crudster",
+        requires=["pymongo (>=3.6.0)", "tornado (>=4.5.3)", "motor (>=1.2.0)"],
+        packages=["crudster"],
+)
